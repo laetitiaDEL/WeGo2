432c48c02cec23e5d87577468b87bae00ca40316
Laetitia <titia.deloirs@hotmail.fr>
2024-01-12 12:06:09 +0100

    "commit 12/01"

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,26 @@
 <template>
     <footer>
-        <nav class="navbar navbar-expand-lg p-1">
+        <nav class="navbar navbar-expand">
             <div class="container-fluid">
-                <router-link to="/"><img src="../assets/logoFooter.svg"></router-link>
+                
                 <div class="collapse navbar-collapse">
                     <div class="navbar-nav">
-                        <router-link to="/home" class="nav-link" aria-current="page" href="#">Accueil</router-link>
-                        <router-link to="/history" class="nav-link" href="#">Mes sorties passées</router-link>
-                        <router-link to="/createOuting" class="nav-link" href="#">Créer une sortie</router-link>
-                        <router-link to="/messaging" class="nav-link" href="#">Messages</router-link>
-                        <router-link to="/myAccount" class="nav-link" href="#">Mon compte</router-link>
-                        <router-link to="/settings" class="nav-link" href="#">Paramètres</router-link>
-                        <router-link to="/terms" class="nav-link" href="#">Conditions générales</router-link>
-                        <router-link to="/privacy" class="nav-link" href="#">Confidentialité</router-link>
-                        <router-link to="/contact" class="nav-link" href="#">Contact</router-link>
+                        <router-link to="/"><img src="../assets/logoFooter.svg"></router-link>
+                        <div class="footer_col">
+                            <router-link to="/home" class="nav-link" aria-current="page" href="#">Accueil</router-link>
+                            <router-link to="/history" class="nav-link" href="#">Mes sorties passées</router-link>
+                            <router-link to="/createOuting" class="nav-link" href="#">Créer une sortie</router-link>
+                        </div>  
+                        <div class="footer_col">  
+                            <router-link to="/messaging" class="nav-link" href="#">Messages</router-link>
+                            <router-link to="/myAccount" class="nav-link" href="#">Mon compte</router-link>
+                            <router-link to="/settings" class="nav-link" href="#">Paramètres</router-link>
+                        </div> 
+                        <div class="footer_col">   
+                            <router-link to="/terms" class="nav-link" href="#">Conditions générales</router-link>
+                            <router-link to="/privacy" class="nav-link" href="#">Confidentialité</router-link>
+                            <router-link to="/contact" class="nav-link" href="#">Contact</router-link>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -26,3 +33,25 @@
         name: "Footer"
     }
 </script>
+
+<style scoped>
+
+nav.navbar{
+    padding: 90px;
+}
+.navbar-nav{
+    display: flex; 
+    justify-content: space-between;
+    width: 100%;
+}
+
+.footer_col{
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+}
+
+a.nav-link{
+    padding: 0;
+}
+</style>
